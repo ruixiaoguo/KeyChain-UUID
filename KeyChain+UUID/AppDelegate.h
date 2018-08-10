578248c00c4147c073070f81d93974ae9525d739
ruixiaoguo <grx0917@sina.com>
2018-08-10 14:13:09 +0800

    钥匙串保存设备信息

---
+++ KeyChain+UUID/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KeyChain+UUID
+//
+//  Created by grx on 2018/8/10.
+//  Copyright © 2018年 grx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KeyChain+UUID/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KeyChain+UUID
+//
+//  Created by grx on 2018/8/10.
+//  Copyright © 2018年 grx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
